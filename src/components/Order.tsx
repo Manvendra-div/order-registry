8ac8de0e6c408bad456081e147b8d05fdf78149d
manvendra-weengage <manvendra@weengage.in>
2025-02-16 17:32:04 +0530

    order info dialog UI

--- src/components/Order.tsx
+++ src/components/Order.tsx
@@ -1,10 +1,9 @@
-import { HoverCard, HoverCardContent, HoverCardTrigger } from "./ui/hovercard";
 import zomatoDark from "@/assets/zomato-dark.png";
 import zomatoLight from "@/assets/zomato-light.png";
 import swiggyDark from "@/assets/swiggy-dark.png";
 import swiggyLight from "@/assets/swiggy-light.png";
 import { Button } from "./ui/button";
-import { Info, Loader2, QrCode, Wallet } from "lucide-react";
+import { Copy, Info, Loader2, QrCode, Wallet } from "lucide-react";
 import { useTheme } from "./ui/theme-provider";
 import { OrderType } from "@/types/order.type";
 import { Label } from "./ui/label";
@@ -24,6 +23,14 @@ import {
   TableRow,
 } from "./ui/table";
 import { format } from "date-fns";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
 
 export default function Order({ order }: { order: OrderType }) {
   const { theme } = useTheme();
@@ -68,8 +75,8 @@ export default function Order({ order }: { order: OrderType }) {
       <span className="text-sm sm:text-base font-thin max-w-32 sm:max-w-48 truncate">
         {order.name}
       </span>
-      <HoverCard openDelay={0}>
-        <HoverCardTrigger asChild>
+      <Dialog>
+        <DialogTrigger asChild>
           <Button
             variant={"secondary"}
             effect={"ringHover"}
@@ -78,15 +85,32 @@ export default function Order({ order }: { order: OrderType }) {
           >
             <Info />
           </Button>
-        </HoverCardTrigger>
-        <HoverCardContent className="p-0 mt-2 overflow-hidden max-w-96">
+        </DialogTrigger>
+        <DialogContent className="pb-0 overflow-hidden w-fit max-w-2xl">
+          <DialogHeader>
+            <DialogTitle>Order Details</DialogTitle>
+            <DialogDescription>
+              <Button
+                size={"sm"}
+                effect={"expandIcon"}
+                icon={Copy}
+                iconPlacement="right"
+                variant={"secondary"}
+                className="select-none h-6 text-xs"
+              >
+                {order.orderId}
+              </Button>
+            </DialogDescription>
+          </DialogHeader>
           <Table>
             <TableCaption className="text-xs pb-4">
               {format(
-                new Date(
-                  order.timeStamp?.seconds * 1000 +
-                    order.timeStamp?.nanoseconds / 1000000,
-                ),
+                "seconds" in order.timeStamp
+                  ? new Date(
+                      order.timeStamp.seconds * 1000 +
+                        order.timeStamp.nanoseconds / 1000000,
+                    )
+                  : (order.timeStamp as Date),
                 "MMMM d, yyyy HH:mm",
               )}
             </TableCaption>
@@ -94,23 +118,23 @@ export default function Order({ order }: { order: OrderType }) {
               <TableRow className="text-xs">
                 <TableHead>Quantity</TableHead>
                 <TableHead className="w-[100px]">Variant</TableHead>
-                <TableHead>Name</TableHead>
+                <TableHead className="w-[100px]">Name</TableHead>
                 <TableHead className=" text-right">Price</TableHead>
               </TableRow>
             </TableHeader>
             <TableBody>
               {order.items.map((item) => (
                 <TableRow key={item.id}>
-                  <TableCell className="text-xs font-semibold text-center">
+                  <TableCell className="text-xs font-semibold text-left text-nowrap">
                     {item.quantity}
                   </TableCell>
-                  <TableCell className="text-xs font-semibold capitalize text-center">
+                  <TableCell className="text-xs font-semibold capitalize text-nowrap">
                     {item.varients.varientName}
                   </TableCell>
                   <TableCell className="text-xs font-semibold">
-                    {item.name}
+                    <span className="text-nowrap">{item.name}</span>
                   </TableCell>
-                  <TableCell className="text-base font-bold text-right">
+                  <TableCell className="text-base font-bold text-right text-nowrap">
                     ₹{item.quantity * item.varients.price}
                   </TableCell>
                 </TableRow>
@@ -121,14 +145,14 @@ export default function Order({ order }: { order: OrderType }) {
                 <TableCell colSpan={3} className="text-base font-semibold">
                   Total
                 </TableCell>
-                <TableCell className="text-lg font-bold">
+                <TableCell className="text-lg font-bold text-right">
                   ₹{order.allTotal}
                 </TableCell>
               </TableRow>
             </TableFooter>
           </Table>
-        </HoverCardContent>
-      </HoverCard>
+        </DialogContent>
+      </Dialog>
 
       <div className="text-xs font-medium flex flex-col justify-center items-end gap-1.5 text-muted-foreground">
         <span className="text-xl sm:text-2xl font-bold text-foreground">
